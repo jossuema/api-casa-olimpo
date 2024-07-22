634729493a0d6f4a21d7daaef51065a7334f8244
jossuema <mmalla1@outlook.com>
2024-07-21 19:23:33 -0500

    Adding CORS config

--- app/main.py
+++ app/main.py
@@ -7,10 +7,19 @@ import jwt
 from sqlalchemy.orm import Session
 from datetime import timedelta
 from . import auth, models, schemas
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
